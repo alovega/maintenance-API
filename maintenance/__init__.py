a680120207f1843a775e1518037a0c09b25b9d1c
alovega <alovegakevin@gmail.com>
2018-06-04 14:12:02 +0300

    added a comment to my api resources

--- maintenance/__init__.py
+++ maintenance/__init__.py
@@ -6,8 +6,8 @@ from maintenance.User import Userregister
 
 app = Flask(__name__)
 api = Api(app)
+"""my api resources """
 api.add_resource(HelloWorld,'/')
 api.add_resource(RequestApi, '/api/v1/request/<int:id>', endpoint = 'requestapi')
 api.add_resource(RequestService, '/api/v1/request', endpoint = 'requestservice')
 api.add_resource(Userregister, '/api/v1/user/<int:id>', endpoint = 'user')
-
