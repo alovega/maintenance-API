75865613dd2a1b8c7298345bd028ef95c23df012
alovega <alovegakevin@gmail.com>
2018-06-06 09:28:13 +0300

    Implements the Resource(user) endpoints

--- maintenance/User.py
+++ maintenance/User.py
@@ -6,59 +6,73 @@ from flask_restful import reqparse
 
 class UserDao(object):
 
-    def __init__(self, id,email, username, password, user_type):
-        self.user_id = id
+    def __init__(self, user_id, email, username, password):
+        self.user_id = user_id
         self.email = email
         self.username = username
         self.password = password
-        self.user_type = user_type
 
 
 users = []
-users.append(UserDao(id=1,email='alovegakevin@gmail.com', username='alovega', password='kev1234', user_type='user'))
-users.append(UserDao(id=2,email='amanda@hotmail.com', username='amandachoxxs', password='amanda4567', user_type='Admin'))
+users.append(UserDao(user_id=1, email='alovegakevin@gmail.com', username='alovega', password='kev1234'))
+users.append(UserDao(user_id=2, email='amanda@hotmail.com', username='amandachoxxs', password='amanda4567'))
 
 resource_fields = {
+    'user_id': fields.Integer,
     'email': fields.String,
     'username': fields.String,
     'password': fields.String,
-    'user_type': fields.String
 }
 reqparse = reqparse.RequestParser()
+reqparse.add_argument('user_id', type=int, required=True,default="",location='json')
 reqparse.add_argument('email', type=str, required=True, help='No user email provided', location='json')
 reqparse.add_argument('username', type=str, required=True, help='please choose username', location='json')
 reqparse.add_argument('password', type=str, required=True, help='include password', location='json')
 
-class Userregister(Resource):
+reqparse_copy = reqparse.copy()
+reqparse_copy.remove_argument('user_id')
+reqparse_copy.remove_argument('email')
+reqparse_copy.add_argument('username', type=str, required=True, help='Invalid username', location='json')
+reqparse_copy.add_argument('password', type=str, required=True, help='Invalid password', location='json')
+
+
+
+class UserRegister(Resource):
     @marshal_with(resource_fields)
     def post(self):
         args = reqparse.parse_args()
-        new_id = len(users)
         user = UserDao(
-            user_id=new_id,
+            user_id= args['user_id'],
             email=args['email'],
             username=args['username'],
             password=args['password'],
-            user_type=args['user']
         )
         users.append(user)
         return user, 201
+        print(user)
 
     @marshal_with(resource_fields)
-    def get(self, id):
+    def get(self):
+        return users
+
+
+class UserLogin(Resource):
+
+    @marshal_with(resource_fields)
+    def post(self):
         for user in users:
-            if (user.user_id == id):
+            args = reqparse_copy.parse_args()
+            if args['username'] == user.username and args['password'] == user.password:
                 return user
         abort(404)
 
     @marshal_with(resource_fields)
     def put(self, id):
         for user in users:
-            if (user.id == id):
+            if (user.user_id == id):
                 args = reqparse.parse_args()
                 user.email = args['email']
                 user.username = args['username']
                 user.password = args['password']
+                users.append(user)
                 return user
-
-        abort(404)
--- maintenance/__init__.py
+++ maintenance/__init__.py
@@ -2,7 +2,10 @@ from flask import Flask
 from flask_restful import Api
 
 from maintenance.Request import HelloWorld, RequestApi, RequestService
-from maintenance.User import Userregister
+from maintenance.User import UserRegister
+from maintenance.User import UserLogin
+
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,4 +13,6 @@ api = Api(app)
 api.add_resource(HelloWorld,'/')
 api.add_resource(RequestApi, '/api/v1/request/<int:id>', endpoint = 'requestapi')
 api.add_resource(RequestService, '/api/v1/request', endpoint = 'requestservice')
-api.add_resource(Userregister, '/api/v1/user/<int:id>', endpoint = 'user')
+api.add_resource(UserRegister, '/api/v1/users', endpoint = 'userregister')
+api.add_resource(UserLogin, '/api/v1/users/login', endpoint= 'userlogin')
+
