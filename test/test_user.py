4d108a788c80d0ab552fbce4e8235f9874002fc1
alovega <alovegakevin@gmail.com>
2018-06-18 09:39:19 +0300

    fixing the commented tests

--- test/test_user.py
+++ test/test_user.py
@@ -20,51 +20,51 @@ class TestUserModel(unittest.TestCase):
         res = self.client.post("/auth/signup",json=request)
         self.assertEqual(res.status_code,202)
 
-    # def test_user_login(self):
-    #     request = {"username":"kevin","password":"1234"}
-    #     res = self.client.post("/auth/login",json=request)
-    #     self.assertEqual(res.status_code,202)
-    #
-    # def test_user_registration_with_empty_email(self):
-    #     request = {"email":"", "username": "kevin", "password": "1234"}
-    #     res = self.client.post ("/auth/signup", json=request)
-    #     self.assertEqual (res.status_code, 500)
-    #
-    #
-    # def test_user_registration_with_empty_username(self):
-    #     request = {"email": "alovegakevin@gmail.com", "username": "", "password": "1234"}
-    #     res = self.client.post ("/auth/signup", json=request)
-    #     self.assertEqual (res.status_code, 404)
-    #
-    # def test_user_registration_with_empty_password(self):
-    #     request = {"email": "alovegakevin@gmail.com", "username": "kevin", "password": ""}
-    #     res = self.client.post ("/auth/signup", json=request)
-    #     self.assertEqual (res.status_code, 500)
-    #
-    # def test_user_registration_with_empty_data(self):
-    #     request = {"email": "", "username": "", "password": ""}
-    #     res = self.client.post ("/auth/signup", json=request)
-    #     self.assertEqual (res.status_code, 404)
-    #
-    # def test_user_login_with_wrong_password(self):
-    #     request = {"username": "kevin", "password": "1222"}
-    #     res = self.client.post ("/auth/login", json=request)
-    #     self.assertEqual (res.status_code, 404)
-    #
-    # def test_user_login_with_wrong_password(self):
-    #     request = {"username": "kev", "password": "1234"}
-    #     res = self.client.post ("/auth/login", json=request)
-    #     self.assertEqual (res.status_code, 404)
-    #
-    # def test_user_can_logout(self):
-    #     request = {"username": "kevin", "password": "1234"}
-    #     res = self.client.post ("/auth/login", json=request)
-    #     self.assertEqual(res.status_code,202)
-    #     access_token = res.json['access_token']
-    #     headers= {"Authorization": "Bearer {0}".format(access_token)}
-    #     res2= self.client.post("/auth/logout",headers=headers)
-    #     self.assertEqual(res2.status_code, 200)
-    #
+    def test_user_login(self):
+        request = {"username":"kevin","password":"1234"}
+        res = self.client.post("/auth/login",json=request)
+        self.assertEqual(res.status_code,202)
+
+    def test_user_registration_with_empty_email(self):
+        request = {"email":"", "username": "kevin", "password": "1234"}
+        res = self.client.post ("/auth/signup", json=request)
+        self.assertEqual (res.status_code, 500)
+
+
+    def test_user_registration_with_empty_username(self):
+        request = {"email": "alovegakevin@gmail.com", "username": "", "password": "1234"}
+        res = self.client.post ("/auth/signup", json=request)
+        self.assertEqual (res.status_code, 404)
+
+    def test_user_registration_with_empty_password(self):
+        request = {"email": "alovegakevin@gmail.com", "username": "kevin", "password": ""}
+        res = self.client.post ("/auth/signup", json=request)
+        self.assertEqual (res.status_code, 500)
+
+    def test_user_registration_with_empty_data(self):
+        request = {"email": "", "username": "", "password": ""}
+        res = self.client.post ("/auth/signup", json=request)
+        self.assertEqual (res.status_code, 404)
+
+    def test_user_login_with_wrong_password(self):
+        request = {"username": "kevin", "password": "1222"}
+        res = self.client.post ("/auth/login", json=request)
+        self.assertEqual (res.status_code, 404)
+
+    def test_user_login_with_wrong_password(self):
+        request = {"username": "kev", "password": "1234"}
+        res = self.client.post ("/auth/login", json=request)
+        self.assertEqual (res.status_code, 404)
+
+    def test_user_can_logout(self):
+        request = {"username": "kevin", "password": "1234"}
+        res = self.client.post ("/auth/login", json=request)
+        self.assertEqual(res.status_code,202)
+        access_token = res.json['access_token']
+        headers= {"Authorization": "Bearer {0}".format(access_token)}
+        res2= self.client.post("/auth/logout",headers=headers)
+        self.assertEqual(res2.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
